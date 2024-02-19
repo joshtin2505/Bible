dabaeb55add0ccbdf505f87b392360c5bb25e543
joshtin2505 <justincastro2505@gmail.com>
2024-02-19 01:04:26 -0500

    Update colors and navigation in CSS and JSX files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'waikawa-gray': {
+          '50': '#f2f7fb',
+          '100': '#e7f0f8',
+          '200': '#d3e2f2',
+          '300': '#b9cfe8',
+          '400': '#9cb6dd',
+          '500': '#839dd1',
+          '600': '#6a7fc1',
+          '700': '#6374ae',
+          '800': '#4a5989',
+          '900': '#414e6e',
+          '950': '#262c40',
+        },
+      }
     },
+    
+  
   },
   plugins: [],
 };
