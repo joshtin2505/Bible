45283e24b8eef63959527b4b1aa106cd9fe7eab0
joshtin2505 <justincastro2505@gmail.com>
2024-02-20 22:29:36 -0500

    Add utils.ts, update globals.css, and modify page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <header className="w-full flex justify-center items-center px-11 py-3">
         <nav className="w-full">
-          <Link className="text-2xl font-bold hover:underline hover:text-waikawa-gray-500" href="/">Biblia</Link>
+          <div className="w-auto">
+            <Link
+              className="flex flex-col border-l-[3px] border-l-Tertiary-Root pl-1 cursor-pointer hover:bg-Complementary-Root"
+              href="/"
+            >
+              <h1 className="text-3xl font-bold text-Primary-Root">
+                Biblia
+              </h1>
+              <p className="text-sm">Palabra De Dios</p>
+            </Link>
+          </div>
           <ul></ul>
         </nav>
       </header>
+
     </main>
-  )
+  );
 }
